663ebe65ddec72557f682ae498bfa72b6e8a130a
tito3333 <nievasnahuel@gmail.com>
2022-09-30 15:12:27 +0200

    finish version

--- prueba.py
+++ prueba.py
@@ -1,34 +1,34 @@
-def arithmetic_arranger(problems):
-
+def arithmetic_arranger(problems, sol = False):
     primero = ""
     segundo = ""
     lineas = ""
     sumar= ""
-    string = ""
-
+    arranged_problems = ""
+    
+# problemas no pueden ser mas de 5
     if len(problems) > 5:
             arranged_problems = "Error: Too many problems."
             return arranged_problems
-            # print("error mas de 5 problemas")
+
+            
 
     for problem in problems:
          num1 = problem.split(" ")[0]
          num2 = problem.split(" ")[1]
-         num3 = problem.split(" ")[2]
-
+         num3 = problem.split(" ")[2] 
+         #problemas no pueden tener otro operador que no sea + o -
          if not (num2 in ("+", "-")):
             arranged_problems = "Error: Operator must be '+' or '-'."
-            
-            # print("error debe ser + o -")
-
+            return arranged_problems
+        #cuentas deben ser solo digitos
          if num1.isdigit() == False:
             arranged_problems = "Error: Numbers must only contain digits."
             return arranged_problems
          elif num3.isdigit() == False:
             arranged_problems = "Error: Numbers must only contain digits."
             return arranged_problems
-           # error de caracter que no se numerico
-            
+
+        #numeros no pueden ser mayor a 5
          if len(num1) >=5 :
             arranged_problems = "Error: Numbers cannot be more than four digits."
             return arranged_problems
@@ -36,104 +36,36 @@ def arithmetic_arranger(problems):
          elif len(num3) >=5 :
             arranged_problems = "Error: Numbers cannot be more than four digits."
             return arranged_problems
+
+         operation = ""
+         if num2 == "+":
+              operation =  str(int(num1) + int(num3))
+         elif num2 == "-":
+              operation =  str(int(num1) - int(num3))
+             
+
+         length = max(len(num1), len(num3)) + 2
+         arriba = str(num1).rjust(length)
+         abajo = num2 + str(num3).rjust(length - 1)
+         linea = "-" * (length)
+         resultado = str(operation).rjust(length)
+
+        
+         primero += arriba + "    "
+         segundo += abajo + "    "
+         lineas += linea + "    "
+         sumar += resultado + "    "
          
 
-
-    
-
-
-
-    
-    # return arranged_problems
+             
+    if sol:
+        arranged_problems = primero + "\n" + segundo + "\n" + lineas + "\n" + sumar
+    else:
+       arranged_problems = primero + "\n" + segundo + "\n" + lineas
 
 
-
-# arithmetic_arranger(["32 + 8", "1 - 3801", "9999 / 9999", "523 - 49", "523 - 49", "523 - 49"])
-
-arithmetic_arranger(["744 + 7", "7 - 3804", "9999 + 9999", "523 + 49"])
- 
+    print (arranged_problems)
 
 
 
-# num = 60
-
-# strin = "hola"
-
-# print (type(num))
-# print (type(strin))
-
-
-
-# def check (a):
-#     if type(a) == str:
-#         print( a + " es un string")
-#     else :
-#         print (str(a) + " Es un numero")
-
-
-
-
-# check(strin)
-# check(num)
-
-
-# num1 = "+"
-# num2= "-"
-# num3 = "*"
-# num4= 5
-# num5 = "7 8 8"
-
-# print(num1)
-# print(num2)
-# print(num3)
-# print (type(num4))
-# print (type(num5))
-
-
-# num6 = num5.split(" ")
-# print(num6)
-
-# num7 = num6[0]
-# num8= num6[1]
-# print(num7)
-# print(num8)
-
-# print (type(num7))
-# print (num7.isdigit())
-# print (num8.isdigit())
-
-# if num7.isdigit() == True:
-#     print("Es un num")
-
-
-# def check (a,b):
-#     if a.isdigit()  == False :
-#         print ("No es un num en la funcion")
-#     elif  b.isdigit() == False:
-#         print ("No es un num en la funcion 2")
-#     else :
-#         print ("es un num fun")
-
-
-# check (num7, num8)
-
-
-# if type(num4) != int:
-#     print("es distinto a un numero")
-# else :
-#     print( "es un nume")
-
-
-# # if (num1 != "+") or (num1 != "-") :
-# #     print("es distino")
-# # else:
-# #     print("es un simbolo")
-
-
-# # if not (operator[0] in ('-','+') ):
-# #         return "Error: Operator must be '+' or '-'."
-
-# if not (num2 in ("+", "-")):
-#     print("es distino")
-# else:
-#     print("es un simbolo")
+arithmetic_arranger(["32 + 698", "1 - 3801", "9999 + 9999", "43 - 49", "523 - 49"])
